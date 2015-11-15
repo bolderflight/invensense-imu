5f4cc5041a226ab6aaeb6e1843d7b490db61be6c
Brian Taylor <brian.taylor@bolderflight.com>
2015-11-15 10:44:01 -0600

    cleaned up read code and added gyros

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -16,60 +16,130 @@ MPU9250::MPU9250(int address){
 }
 
 /* starts the I2C communication */
-void MPU9250::begin(){
-  Wire.begin(I2C_MASTER, 0, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);  // starting the I2C
-  writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_4G); // setting the accel range
-  //setGyroRange(gyroRange); // setting the gyro range
+void MPU9250::begin(String accelRange, String gyroRange){
+  Wire.begin(I2C_MASTER, 0, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400); // starting the I2C
+
+  writeRegister(PWR_MGMNT_1,CLOCK_SEL_PLL); // select clock source to gyro
+
+  if(accelRange.equals("2G")){
+    writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_2G); // setting the accel range to 2G
+    _accelScale = G * 2.0/32767.5; // setting the accel scale to 2G
+  }
+
+  if(accelRange.equals("4G")){
+    writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_4G); // setting the accel range to 4G
+    _accelScale = G * 4.0/32767.5; // setting the accel scale to 4G
+  }
+
+  if(accelRange.equals("8G")){
+    writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_8G); // setting the accel range to 8G
+    _accelScale = G * 8.0/32767.5; // setting the accel scale to 8G
+  }
+
+  if(accelRange.equals("16G")){
+    writeRegister(ACCEL_CONFIG,ACCEL_FS_SEL_16G); // setting the accel range to 16G
+    _accelScale = G * 16.0/32767.5; // setting the accel scale to 16G
+  }
+
+  if(gyroRange.equals("250DPS")){
+    writeRegister(GYRO_CONFIG,GYRO_FS_SEL_250DPS); // setting the gyro range to 250DPS
+    _gyroScale = 250.0/32767.5; // setting the gyro scale to 250DPS
+  }
+
+  if(gyroRange.equals("500DPS")){
+    writeRegister(GYRO_CONFIG,GYRO_FS_SEL_500DPS); // setting the gyro range to 500DPS
+    _gyroScale = 500.0/32767.5; // setting the gyro scale to 500DPS
+  }
+
+  if(gyroRange.equals("1000DPS")){
+    writeRegister(GYRO_CONFIG,GYRO_FS_SEL_1000DPS); // setting the gyro range to 1000DPS
+    _gyroScale = 1000.0/32767.5; // setting the gyro scale to 1000DPS
+  }
+
+  if(gyroRange.equals("2000DPS")){
+    writeRegister(GYRO_CONFIG,GYRO_FS_SEL_2000DPS); // setting the gyro range to 2000DPS
+    _gyroScale = 2000.0/32767.5; // setting the gyro scale to 2000DPS
+  }
 }
 
-uint8_t MPU9250::readRegister(uint8_t registerAddress){
-  uint8_t avail;
-
-  // write to register to be read
-  Wire.beginTransmission(_address);
-  Wire.write(registerAddress);
-  Wire.endTransmission();
-
-  // request 1 byte back
-  Wire.requestFrom(_address,1);
-
-  // check to see how many bytes are available
-  avail = Wire.available();
-  if(avail == 1){
-    return Wire.read();
-  }
-  else{
-    return 0;
-  }
-}
-
+/* writes a register to MPU9250 given a register address and data */
 void MPU9250::writeRegister(uint8_t subAddress, uint8_t data){
-  Wire.beginTransmission(_address);
-  Wire.write(subAddress);
-  Wire.write(data);
+  Wire.beginTransmission(_address); // open the device
+  Wire.write(subAddress); // write the register address
+  Wire.write(data); // write the data
   Wire.endTransmission();
 }
 
-uint16_t MPU9250::readRegisters(uint8_t msbAddress, uint8_t lsbAddress){
+/* reads registers from MPU9250 given a starting register address, number of bytes, and a pointer to store data */
+void MPU9250::readRegisters(uint8_t subAddress, int count, uint8_t* dest){
+  Wire.beginTransmission(_address); // open the device
+  Wire.write(subAddress); // specify the starting register address
+  Wire.endTransmission(false);
 
-  // write to register to be read
-  Wire.beginTransmission(_address);
-  Wire.write(msbAddress);
-  Wire.endTransmission();
+  Wire.requestFrom(_address, count); // specify the number of bytes to receive
 
-  // request 1 byte back
-  Wire.requestFrom(_address,1,false);
-  uint8_t msb = Wire.read();
-  Wire.requestFrom(_address,1);
-  uint8_t lsb = Wire.read();
-
-  return (((uint16_t)msb) << 8) | lsb;
+  uint8_t i = 0; // read the data into the buffer
+  while(Wire.available()){
+    dest[i++] = Wire.read();
+  }
 }
 
+/* get accelerometer data given pointers to store the three values */
 void MPU9250::getAccel(double* ax, double* ay, double* az){
+  uint8_t buff[6];
+  uint16_t accel[3];
 
-  *ax = ((int16_t) readRegisters(ACCEL_XOUT_H, ACCEL_XOUT_L)) * G * 4.0/32767.5;
-  *ay = ((int16_t) readRegisters(ACCEL_YOUT_H, ACCEL_YOUT_L)) * G * 4.0/32767.5;
-  *az = ((int16_t) readRegisters(ACCEL_ZOUT_H, ACCEL_ZOUT_L)) * G * 4.0/32767.5;
+  readRegisters(ACCEL_OUT, sizeof(buff), &buff[0]); // grab the data from the MPU9250
+
+  accel[0] = (((uint16_t)buff[0]) << 8) | buff[1];  // combine into 16 bit values
+  accel[1] = (((uint16_t)buff[2]) << 8) | buff[3];
+  accel[2] = (((uint16_t)buff[4]) << 8) | buff[5];
+
+  *ax = ((int16_t) accel[0]) * _accelScale; // typecast and scale to values
+  *ay = ((int16_t) accel[1]) * _accelScale;
+  *az = ((int16_t) accel[2]) * _accelScale;
 }
+
+/* get gyro data given pointers to store the three values */
+void MPU9250::getGyro(double* gx, double* gy, double* gz){
+  uint8_t buff[6];
+  uint16_t gyro[3];
+
+  readRegisters(GYRO_OUT, sizeof(buff), &buff[0]); // grab the data from the MPU9250
+
+  gyro[0] = (((uint16_t)buff[0]) << 8) | buff[1];  // combine into 16 bit values
+  gyro[1] = (((uint16_t)buff[2]) << 8) | buff[3];
+  gyro[2] = (((uint16_t)buff[4]) << 8) | buff[5];
+
+  *gx = ((int16_t) gyro[0]) * _gyroScale; // typecast and scale to values
+  *gy = ((int16_t) gyro[1]) * _gyroScale;
+  *gz = ((int16_t) gyro[2]) * _gyroScale;
+}
+
+/* get accelerometer and gyro data given pointers to store values */
+void MPU9250::getMotion6(double* ax, double* ay, double* az, double* gx, double* gy, double* gz){
+  uint8_t buff[14];
+  uint16_t accel[3];
+  uint16_t gyro[3];
+
+  readRegisters(ACCEL_OUT, sizeof(buff), &buff[0]); // grab the data from the MPU9250
+
+  accel[0] = (((uint16_t)buff[0]) << 8) | buff[1];  // combine into 16 bit values
+  accel[1] = (((uint16_t)buff[2]) << 8) | buff[3];
+  accel[2] = (((uint16_t)buff[4]) << 8) | buff[5];
+
+  gyro[0] = (((uint16_t)buff[8]) << 8) | buff[9];
+  gyro[1] = (((uint16_t)buff[10]) << 8) | buff[11];
+  gyro[2] = (((uint16_t)buff[12]) << 8) | buff[13];
+
+  *ax = ((int16_t) accel[0]) * _accelScale; // typecast and scale to values
+  *ay = ((int16_t) accel[1]) * _accelScale;
+  *az = ((int16_t) accel[2]) * _accelScale;
+
+  *gx = ((int16_t) gyro[0]) * _gyroScale;
+  *gy = ((int16_t) gyro[1]) * _gyroScale;
+  *gz = ((int16_t) gyro[2]) * _gyroScale;
+}
+
+
 
--- MPU9250.h
+++ MPU9250.h
@@ -2,7 +2,7 @@
 // title:     MPU9250.h
 // author:    Taylor, Brian R.
 // email:     brian.taylor@bolderflight.com
-// date:      2015-11-07 
+// date:      2015-11-15 
 // license: 
 //
 
@@ -11,12 +11,8 @@
 
 #include "Arduino.h"
 
-#define ACCEL_XOUT_H		0x3B
-#define ACCEL_XOUT_L		0x3C
-#define ACCEL_YOUT_H		0x3D
-#define ACCEL_YOUT_L		0x3E
-#define ACCEL_ZOUT_H		0x3F
-#define ACCEL_ZOUT_L		0x40
+#define ACCEL_OUT		0x3B
+#define GYRO_OUT		0x43
 
 #define ACCEL_CONFIG		0x1C
 #define ACCEL_FS_SEL_2G		0x00
@@ -24,21 +20,34 @@
 #define ACCEL_FS_SEL_8G 	0x10
 #define ACCEL_FS_SEL_16G	0x18
 
-#define G			9.807						
+#define GYRO_CONFIG		0x1A
+#define GYRO_FS_SEL_250DPS	0x00
+#define GYRO_FS_SEL_500DPS	0x08
+#define GYRO_FS_SEL_1000DPS	0x10
+#define GYRO_FS_SEL_2000DPS	0x18
+
+#define PWR_MGMNT_1		0x6B
+#define CLOCK_SEL_PLL		0x01
+
+#define G			9.807
+#define D2R			180.0/PI						
 
 
 
 class MPU9250{
   public:
     MPU9250(int address);
-    void begin();
+    void begin(String accelRange, String gyroRange);
     void getAccel(double* ax, double* ay, double* az);
-    uint8_t readRegister(uint8_t registerAddress);
+    void getGyro(double* gx, double* gy, double* gz);
+    //void getMag(double* hx, double* hy, double* hz);
+    void getMotion6(double* ax, double* ay, double* az, double* gx, double* gy, double* gz);
   private:
     int _address;
+    double _accelScale;
+    double _gyroScale;
     void writeRegister(uint8_t subAddress, uint8_t data);
-
-    uint16_t readRegisters(uint8_t msbAddress, uint8_t lsbAddress);
+    void readRegisters(uint8_t subAddress, int count, uint8_t* dest);
 };
 
 #endif
