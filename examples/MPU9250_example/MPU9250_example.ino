9bd952b0dc6c6f365da851b179e7165e03ac8eb7
Brian Taylor <brian.taylor@bolderflight.com>
2016-08-31 13:42:08 -0700

    Added MIT license.

--- examples/MPU9250_example/MPU9250_example.ino
+++ examples/MPU9250_example/MPU9250_example.ino
@@ -1,10 +1,26 @@
-//
-// title:     MPU9250_example.ino
-// author:    Taylor, Brian R.
-// email:     brian.taylor@bolderflight.com
-// date:      2015-11-19 
-// license: 
-//
+/*
+MPU9250_example.ino
+Brian R Taylor
+brian.taylor@bolderflight.com
+2015-11-19 
+
+Copyright (c) 2016 Bolder Flight Systems
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include "MPU9250.h"
 #include <i2c_t3.h> // I2C library
