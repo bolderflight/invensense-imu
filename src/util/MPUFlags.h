37c0469d44aa88f3f8d87ed300dd9492a640a1c9
Pat Deegan <pat.github@psychogenic.net>
2018-04-12 08:10:40 -0400

    Created MPUFlags.h as a centralized location for autodetected compile-time flags (e.g. some platforms don't have a setClock() in their I2C lib).

---
+++ src/util/MPUFlags.h
@@ -0,0 +1,8 @@
+#ifndef MPU9250_UTILS_FLAGS_H_
+#define MPU9250_UTILS_FLAGS_H_
+
+#ifdef __Simblee__
+#define MPU9250_API_I2C_SETCLOCK_UNAVAILABLE
+#endif
+
+#endif
--- src/util/MPUUtils.h
+++ src/util/MPUUtils.h
@@ -1,6 +1,7 @@
 #ifndef MPU9250_UTILS_MAININC_HEADER_
 #define MPU9250_UTILS_MAININC_HEADER_
 
+#include "MPUFlags.h"
 #include "MadgwickAHRS.h"
 
 #endif /* MPU9250_UTILS_MAININC_HEADER_ */
