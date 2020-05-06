d25f356884e323d5b3b856e0f98336f847a285ed
Brian Taylor <brian.taylor@bolderflight.com>
2020-05-06 16:32:46 -0600

    Adding more tests

--- tests/slave.cc
+++ tests/slave.cc
@@ -10,6 +10,7 @@
 #include "remote_test/remote_test.h"
 #include "serial_link/serial_link.h"
 #include "mcu_hil_defs/mcu_hil_defs.h"
+#include <math.h>
 
 /* Test the ability to begin communications over SPI */
 bool TestBeginSpi() {
@@ -27,9 +28,88 @@ bool TestBeginI2c() {
 
 /* Test data collection over I2C */
 
+/* Test accel range */
+bool CheckAccel(Accel accel) {
+  float thresh = 0.5f;
+  if (fabs(accel.x_g()) > thresh) {
+    return false;
+  }
+  if (fabs(accel.y_g()) > thresh) {
+    return false;
+  }
+  if (fabs(accel.z_g()) > (1.0f + thresh)) {
+    return false;
+  }
+  if (fabs(accel.z_g()) < (1.0f - thresh)) {
+    return false;
+  }
+  return true;
+}
+bool TestAccelRange(sensors::Mpu9250 &mpu) {
+  delay(2);
+  if (!mpu.accel_range(sensors::Mpu9250::ACCEL_RANGE_2G)) {
+    return false;
+  }
+  if (!mpu.Read()) {
+    return false;
+  }
+  Imu imu = mpu.imu();
+  if (!CheckAccel(imu.accel)) {
+    return false;
+  }
+  delay(2);
+  if (!mpu.accel_range(sensors::Mpu9250::ACCEL_RANGE_4G)) {
+    return false;
+  }
+  if (!mpu.Read()) {
+    return false;
+  }
+  imu = mpu.imu();
+  if (!CheckAccel(imu.accel)) {
+    return false;
+  }
+  delay(2);
+  if (!mpu.accel_range(sensors::Mpu9250::ACCEL_RANGE_8G)) {
+    return false;
+  }
+  if (!mpu.Read()) {
+    return false;
+  }
+  imu = mpu.imu();
+  if (!CheckAccel(imu.accel)) {
+    return false;
+  }
+  delay(2);
+  if (!mpu.accel_range(sensors::Mpu9250::ACCEL_RANGE_16G)) {
+    return false;
+  }
+  if (!mpu.Read()) {
+    return false;
+  }
+  imu = mpu.imu();
+  if (!CheckAccel(imu.accel)) {
+    return false;
+  }
+  return true; 
+}
 /* Test accel range SPI */
-
+bool TestAccelRangeSpi() {
+  sensors::Mpu9250 mpu(&MPU9250_SPI, MPU9250_SPI_CS);
+  bool status = mpu.Begin();
+  if (!status) {
+    return false;
+  }
+  return TestAccelRange(mpu);
+}
 /* Test accel range I2C */
+bool TestAccelRangeI2c() {
+  sensors::Mpu9250 mpu(&MPU9250_I2C, MPU9250_I2C_ADDR);
+  bool status = mpu.Begin();
+  if (!status) {
+    return false;
+  }
+  return TestAccelRange(mpu);
+}
 
 /* Test gyro range SPI */
 
@@ -43,6 +123,10 @@ bool TestBeginI2c() {
 
 /* Test DLPF I2C */
 
+/* Test rotation SPI */
+
+/* Test rotation I2C */
+
 
 int main() {
   /* Starting the remote test interface */
@@ -51,6 +135,8 @@ int main() {
   /* Registering tests */
   test.AddTest(1, TestBeginSpi);
   test.AddTest(2, TestBeginI2c);
+  test.AddTest(5, TestAccelRangeSpi);
+  test.AddTest(6, TestAccelRangeI2c);
   while (1) {
     /* Check for new tests */
     test.Check();
