e4dd08aa0b6655510859448b05d660ec35a2f6e7
Brian Taylor <brian.taylor@bolderflight.com>
2020-05-09 14:22:31 -0600

    Debugging accel range test

--- tests/slave.cc
+++ tests/slave.cc
@@ -37,7 +37,7 @@ bool CheckAccel(Accel accel) {
   if (fabs(accel.y_g()) > thresh) {
     return false;
   }
-  if (fabs(accel.z_g()) > (-1.0f + thresh)) {
+  if (fabs(accel.z_g()) > (1.0f + thresh)) {
     return false;
   }
   // if (fabs(accel.z_g()) < (-1.0f - thresh)) {
